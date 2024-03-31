c76de226e843a878273b9560d6b936aaf0a18f74
Emran-Ali <emranitmbstu@gmail.com>
2024-03-31 12:39:43 +0600

    carts functinality

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,15 +4,30 @@ import MenuAppBar from "../component/MenuAppBar";
 
 function MainLayout() {
   const [addedItems, setAddedItem] = useState([]);
+  const addQuantity = (id) => {
+    const AllProduct = [...addedItems];
+    AllProduct[id].quantity += 1;
+    setAddedItem([...AllProduct]);
+  };
+  const delQuantity = (id) => {
+    let AllProduct = [...addedItems];
+    AllProduct[id].quantity -= 1;
+    if (AllProduct[id].quantity <= 0) {
+      AllProduct.splice(id, 1);
+      console.log(AllProduct);
+    }
+    setAddedItem([...AllProduct]);
+  };
+
   const addItem = (product) => {
-    product.addNumber = 1;
-    const itemArr = addedItems;
-    setAddedItem([...itemArr, product]);
+    const pro = addedItems.filter((obj) => obj.id !== product.id);
+    product.quantity = product.quantity ? Number(product.quantity) + 1 : 1;
+    setAddedItem([...pro, product]);
   };
 
   return (
     <>
-      <MenuAppBar items={addedItems} />
+      <MenuAppBar items={addedItems} add={addQuantity} del={delQuantity} />
       <Outlet context={addItem} />
     </>
   );
